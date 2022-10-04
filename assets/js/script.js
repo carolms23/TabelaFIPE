885cf80bf7bb8c398bc2e18863765decbb2b2e19
carolms23 <carolinems360@gmail.com>
2022-10-03 22:21:24 -0300

    adição de estilo select

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,41 @@ fetch(url)
             const option = document.createElement('option')
             option.value = item.valor
             option.innerText = item.nome
-            const select = document.getElementById("montadora--carro")
+            const select = document.getElementById("#montadora-carro")
             select.append(option)
         })
     })
     .catch(err => {
         console.log("Failed to access data",err)
     })
+
+var mySelect = new BVSelect({
+    selector: "#montadora-carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
+var mySelect = new BVSelect({
+    selector: "#modelo--carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
+var mySelect = new BVSelect({
+    selector: "#ano--carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
