995443bc22959c1008fe5320ae5233a532eec23e
carolms23 <carolinems360@gmail.com>
2023-01-11 22:07:00 -0300

    refatoraçao da funcao de request com axios e estilo do select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,41 @@
-
+import axios from 'axios'
 const url  = 'https://brasilapi.com.br/api/fipe/marcas/v1/carros'
 
-//request json body
-fetch(url)
-    .then(async function (response) {
-        const json = await response.json()
-        json.forEach(item => {
-            const option = document.createElement('option')
-            option.value = item.valor
-            option.innerText = item.nome
-            const select = document.getElementById("#montadora-carro")
-            select.append(option)
-        })
-    })
-    .catch(err => {
-        console.log("Failed to access data",err)
-    })
 
-var mySelect = new BVSelect({
-    selector: "#montadora-carro",
-    offset: true,
-    width: "30rem",
-    searchbox : false,
-    search_autofocus : false,
-    offset : true,
-    search_placeholder :"Pesquisa...",
-    placeholder : "Selecione uma opção",
-});
-var mySelect = new BVSelect({
-    selector: "#modelo--carro",
-    offset: true,
-    width: "30rem",
-    searchbox : false,
-    search_autofocus : false,
-    offset : true,
-    search_placeholder :"Pesquisa...",
-    placeholder : "Selecione uma opção",
-});
-var mySelect = new BVSelect({
-    selector: "#ano--carro",
-    offset: true,
-    width: "30rem",
-    searchbox : false,
-    search_autofocus : false,
-    offset : true,
-    search_placeholder :"Pesquisa...",
-    placeholder : "Selecione uma opção",
-});
+//request json body
+// fetch(url)
+//     .then(async function (response) {
+//         const json = await response.json()
+//         json.forEach(item => {
+//             const option = document.createElement('option')
+//             option.value = item.valor
+//             option.innerText = item.nome
+//             const select = document.getElementById("#montadora-carro")
+//             select.append(option)
+//         })
+//     })
+//     .catch(err => {
+//         console.log("Failed to access data",err)
+//     })
+
+async function fetchResponse(){
+    try {
+        const listaMontadora = await axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+        console.log(listaMontadora.data)
+    } catch (error) {
+        console.log()
+    }
+   
+}
+
+function addOption(){
+    listaMotadora.array.forEach(marca => {
+        const option = document.createElement('option')
+        option.value = marca.nome
+        option.innerHTML = marca.name
+        const select = document.getElementById('#montadora-carro')
+        select.append(option)
+    });
+}
+
+fetchResponse()
---
+++ assets/js/selects.js
@@ -0,0 +1,30 @@
+var mySelect = new BVSelect({
+    selector: "#montadora-carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
+var mySelect = new BVSelect({
+    selector: "#modelo--carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
+var mySelect = new BVSelect({
+    selector: "#ano--carro",
+    offset: true,
+    width: "30rem",
+    searchbox : false,
+    search_autofocus : false,
+    offset : true,
+    search_placeholder :"Pesquisa...",
+    placeholder : "Selecione uma opção",
+});
